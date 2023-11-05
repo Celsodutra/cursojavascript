1786614db1c3b4affd2594949eb57762f1c17153
Celso Dutra <celsoaugsto@gmail.com>
2023-11-05 02:49:13 -0300

    Split

--- aula-01.js
+++ aula-01.js
@@ -10,12 +10,3 @@ console.log('O resultado é', x+y);
 console.log('O resultado é', soma);
 console.log('O resultado é', subtração);
 console.log('O resultado é', divisão);
-
-const numero1 = 1;
-let numero2 = 2;
-
-if(numero1 === 1) {
-    numero2 = 10
-}
-
-console.log('O resultado é', numero2)
