c0679be52220e4b47e1f8da5b5004ffcb539eea4
Celso Dutra <celsoaugsto@gmail.com>
2023-11-05 14:15:02 -0300

    Novas mudanças

---
+++ aula-03.js
@@ -0,0 +1,11 @@
+const idade = 8;
+let podeEntrar = false;
+
+if(idade >= 18) {
+    podeEntrar = true;
+
+} else {
+    console.log('O usuário não pode entrar!')
+} 
+
+console.log('O usuário pode entrar', podeEntrar)
