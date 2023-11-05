dc310e258c26086b4fe4cd843efa46f7f1e2db97
Celso Dutra <celsoaugsto@gmail.com>
2023-11-05 01:15:02 -0300

    Primeiro commit

---
+++ aula-01.js
@@ -0,0 +1,10 @@
+console.log('Hello Fucker');
+
+const x = 1;
+const y = -3;
+const soma = x+y;
+const subtração = x-y;
+
+console.log('O resultado é', x+y);
+console.log('O resultado é', soma);
+console.log('O resultado é', subtração);
