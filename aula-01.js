8cac2cd94429d3717e3bfb6403926dc8b2ad126f
Celso Dutra <celsoaugsto@gmail.com>
2023-11-05 01:53:55 -0300

    Segundo commit

--- aula-01.js
+++ aula-01.js
@@ -4,7 +4,9 @@ const x = 1;
 const y = -3;
 const soma = x+y;
 const subtração = x-y;
+const divisão = x/y;
 
 console.log('O resultado é', x+y);
 console.log('O resultado é', soma);
 console.log('O resultado é', subtração);
+console.log('O resultado é', divisão);
