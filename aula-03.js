db756a74b695a6692b020dcd1abbd994f14453c6
Celso Dutra <celsoaugsto@gmail.com>
2023-11-06 01:10:19 -0300

    Atualizar

--- aula-03.js
+++ aula-03.js
@@ -1,11 +1,16 @@
 const idade = 8;
+const nome = 'Celso';
 let podeEntrar = false;
 
-if(idade >= 18) {
+if(idade >= 18 && nome === 'Celso') {
     podeEntrar = true;
+    console.log ('O usuário pode entrar!')
 
 } else {
     console.log('O usuário não pode entrar!')
 } 
 
-console.log('O usuário pode entrar', podeEntrar)
+
+// "||" é uma conjunção alternativa e "&&"" é um bicondicional.
+// "=" é atribuição, "==" é um comparativo, "===" é um comparativo mais preciso.
+// Também temos "<", ">", ">=" e "<=".
